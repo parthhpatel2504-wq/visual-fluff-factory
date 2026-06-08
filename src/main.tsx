cb86a5674cc856e07c755e03c55e6ef464e30ad4
parthhpatel2504-wq <parthhpatel2504@gmail.com>
2026-06-08 12:46:47 +0530

    Change import path for App component to TypeScript file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import App from "./routes/index";
+import App from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
